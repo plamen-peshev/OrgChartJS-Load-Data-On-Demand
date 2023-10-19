40f3dcc0c764a8227e4e4ea942c71838fa24bbd5
plamen-peshev <plamen.peshev@live.com>
2023-10-19 11:29:02 +0300

    Add project files.

---
+++ OrgChartJSLoadDataOnDemand/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CSharp.RuntimeBinder;
+using System.Diagnostics;
+using System.Text.Json;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace OrgChartJSLoadDataOnDemand.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<dynamic> nodes = new List<dynamic> 
+        { 
+            new { id = 1 },
+            new { id = 2, pid = 1 },
+            new { id = 3, pid = 1 },
+            new { id = 4, pid = 2 },
+            new { id = 5, pid = 2 },
+            new { id = 6, pid = 5 },
+            new { id = 7, pid = 5 },
+            new { id = 8, pid = 6 },
+            new { id = 9, pid = 6 },
+            new { id = 10, pid = 8 },
+            new { id = 11, pid = 9 },
+            new { id = 12, pid = 9 },
+            new { id = 13, pid = 6 },
+            new { id = 14, pid = 13 },
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult FetchNodes(int? id)
+        {
+            var demandedNodes = new List<dynamic>();
+            var nodeIdsWithChildren = new List<int>();
+            if (id == null)
+            {
+                var node = nodes.First();
+                demandedNodes.Add(node);
+            }
+            else 
+            {
+                foreach (var node in nodes)
+                {
+                    try
+                    {
+                        if (node.pid == id)
+                        {
+                            demandedNodes.Add(node);
+                        }
+                    }
+                    catch (RuntimeBinderException)
+                    {
+                        //pid doesn't exist
+                    }
+                }
+            }
+
+            foreach (var node in nodes)
+            {
+                if (HasChildren(node.id))
+                {
+                    nodeIdsWithChildren.Add(node.id);
+                }
+            }
+ 
+            return Json(new 
+            {
+                demandedNodes = demandedNodes,
+                nodeIdsWithChildren = nodeIdsWithChildren
+            });
+        }
+
+        public bool HasChildren(int id)
+        {
+            foreach (var node in nodes)
+            {
+                try
+                {
+                    if (node.pid == id)
+                    {
+                        return true;
+                    }
+                }
+                catch (RuntimeBinderException)
+                {
+                    //pid doesn't exist
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ OrgChartJSLoadDataOnDemand/wwwroot/site.js
@@ -0,0 +1,66 @@
+﻿OrgChart.templates.ana.expandButton = `<g data-expand-button transform="matrix(1,0,0,1,110,105)" style="cursor:pointer;">
+${OrgChart.templates.ana.plus}
+</g>`;
+
+var chart = new OrgChart(document.getElementById("tree"), {
+    template: 'ana',
+    enableSearch: false,
+    nodeBinding: {
+        field_0: "id",
+        field_1: "pid",
+        expandButton: 'expandButton'
+    }
+});
+
+var responseData = null;
+var expandIdsWithLoadOnDemand = [];
+
+fetch('/home/FetchNodes').then(response => response.json()).then(function (data) {    
+    responseData = data;
+    chart.load(responseData.demandedNodes);
+});
+
+chart.onNodeClick(function (args) { 
+    var that = this;
+    var target = args.event.target;
+    while (target && target.hasAttribute && !target.hasAttribute('data-expand-button')) {
+        target = target.parentNode;
+    }
+    if (target && target.hasAttribute) {
+        fetch('/home/FetchNodes?id=' + args.node.id).then(response => response.json()).then(function (data) {
+            responseData = data;
+            for (var i = 0; i < responseData.demandedNodes.length; i++) {
+                var node = responseData.demandedNodes[i];
+                that.config.nodes.push(node);
+                expandIdsWithLoadOnDemand.push(node.id);
+            }
+
+            that.expandCollapse(args.node.id, expandIdsWithLoadOnDemand, []);
+        });
+    }
+    return false;
+});
+
+//you can remove render event listener if you are happy with the defailt opacity animation
+chart.on('render', function (sender, args) {
+    if (args.res.animations.length) {
+        for (var i = 0; i < args.res.animations[0].length; i++) {
+            var id = args.res.animations[0][i];
+            if (expandIdsWithLoadOnDemand.indexOf(id) != -1) {
+                var node = sender.getNode(id);
+                args.res.animations[1][i].transform = [1, 0, 0, 1, node.parent.x, node.parent.y];
+                args.res.animations[2][i].transform = [1, 0, 0, 1, node.x, node.y];
+            }            
+        }
+    }
+    expandIdsWithLoadOnDemand = [];
+})
+
+chart.onField(function (args) {
+    if (responseData.nodeIdsWithChildren.has(args.node.id)) {
+        if (args.name == 'expandButton') {
+            args.value = 'show';
+        }
+    }
+});
+
